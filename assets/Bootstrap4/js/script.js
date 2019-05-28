99750ce9cfcde9558bc3eea9f2bcd9cfdfbeaf82
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-05-28 02:10:09 -0700

    Update setup boostrap4 Environment

---
+++ assets/Bootstrap4/js/script.js
@@ -0,0 +1,201 @@
+﻿
+/*================== Read More Text ==================*/
+
+$(function () {
+    var showChar = 260;
+    var moretext = "Read More +";
+    var lesstext = "Read Less -";
+    $('.comments-space').each(function () {
+        var content = $(this).html();
+        if (content.length > showChar) {
+            var show_content = content.substr(0, showChar);
+            var hide_content = content.substr(showChar, content.length - showChar);
+            var html = show_content +
+                '<span class="remaining-contents"><span>' +
+                hide_content +
+                '</span>' +
+
+                '<a href="" class="morelink read-more mt-15" style="display:block;">' +
+                moretext +
+                '</a>' + '</span>';;
+
+            $(this).html(html);
+        }
+    });
+
+    $(".morelink").click(function () {
+
+        if ($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+});
+
+
+/*===================== Smooth Scrolling ======================*/
+$(function () {
+    $('a').smoothScroll();
+});
+/*======================= Nav Active Class =======================*/
+
+$(function () {
+    $('.nav-item').on('click',
+        function () {
+            $('.nav-item').removeClass('active');
+            $(this).addClass('active');
+        });
+});
+
+$(function () {
+    $('#work .btn-filter').on('click',
+        function () {
+            $('.btn-filter').removeClass('active-bg');
+            $(this).addClass('active-bg');
+        });
+});
+
+/*===================== Load More Images ======================*/
+$(document).ready(function () {
+
+    $('.loadMore').loadMoreResults({
+        displayedItems: 8,
+        showItems: 4
+    });
+
+
+});
+
+/*===================== Owl Carousel Slider ======================*/
+//Init the carousel
+$(function () {
+    $('#owl-one').owlCarousel({
+        loop: true,
+        margin: 20,
+        dots: false,
+        nav: true,
+        slideBy: 3,
+        item: 3,
+        navText: ["<i> <img src='assets/Images/attr/slider-dot.png' class='arrow-img' /> </i>", "<i> <img src='assets/Images/attr/slider-dot.png' class='arrow-img' /> </i>"],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                dots: true,
+                nav: false
+            },
+            400: {
+                items: 1,
+                dots: true,
+                nav: false
+            },
+            600: {
+                items: 2,
+                dots: false,
+                nav: true
+            },
+            768: {
+                items: 2,
+                dots: false,
+                nav: true
+            },
+            830: {
+                items: 2,
+                dots: false,
+                nav: true
+            },
+            1000: {
+                items: 3,
+                dots: false,
+                nav: true
+            }
+        }
+    });
+});
+$(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        dots: false,
+        nav: true,
+        slideBy: 1,
+        item: 1,
+        navText: ["<i class='flaticon-last-track-left-arrow'></i>", "<i class='flaticon-right-arrow'> </i>"],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                dots: true,
+                nav: false
+            },
+            400: {
+                items: 1,
+                dots: true,
+                nav: false
+            },
+            600: {
+                items: 1,
+                dots: false,
+                nav: true
+            },
+            768: {
+                items: 1,
+                dots: false,
+                nav: true
+            },
+            830: {
+                items: 1,
+                dots: false,
+                nav: true
+            },
+            1000: {
+                items: 1,
+                dots: false,
+                nav: true
+            }
+        }
+    });
+});
+
+/*===================== Scroll Top Function Script ======================*/
+$(function () {
+
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 100) {
+            $("nav").addClass("nav-bg-color");
+
+        } else {
+
+            $("nav").removeClass("nav-bg-color");
+
+
+        }
+
+
+    });
+});
+
+/*===================== Another Load More Script ======================*/
+$(function () {
+    $(".no-display").slice(0, 4).show();
+    $("#load-more").on('click', function (e) {
+        e.preventDefault();
+        $(".no-display:hidden").slice(0, 2).slideDown();
+        if ($(".no-display:hidden").length == 0) {
+            $("#load-more").fadeOut('slow');
+
+
+        }
+        $('html,body').animate({
+            scrollTop: $(this).offset().center
+        }, 1500);
+    });
+});
+/*===================== Filter Section Script ======================*/
